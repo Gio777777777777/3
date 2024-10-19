470216d4cfa6e8f2cf1260c6341eb7bc10e87687
giorgi cqvitaria <cqvitariagiorgi12@gmail.com>
2024-10-19 16:36:05 +0400

    first commit

---
+++ src/UserData.js
@@ -0,0 +1,48 @@
+const users = [
+    {
+        id: 1,
+        name: "Giorgi",
+        surname: "cqvitaria",
+        nickname: "gio",
+        age: 29,
+        img_url: "https://randomuser.me/api/portraits/men/5.jpg",
+        email: "john.doe@example.com",
+        occupation: "Developer",
+        location: "New York, USA"
+    },
+    {
+        id: 2,
+        name: "sandro",
+        surname: "ungiaze",
+        nickname: "ungo",
+        age: 20,
+        img_url: "https://randomuser.me/api/portraits/men/93.jpg",
+        email: "jane.smith@example.com",
+        occupation: "Designer",
+        location: "London, UK"
+    },
+    {
+        id: 3,
+        name: "nikoloz",
+        surname: "zibzibaze",
+        nickname: "nika",
+        age: 35,
+        img_url: "https://randomuser.me/api/portraits/men/38.jpg",
+        email: "alice.johnson@example.com",
+        occupation: "Project Manager",
+        location: "Berlin, Germany"
+    },
+    {
+        id: 4,
+        name: "dachi",
+        surname: "shengelia",
+        nickname: "dachi",
+        age: 22,
+        img_url: "https://randomuser.me/api/portraits/men/69.jpg",
+        email: "bob.williams@example.com",
+        occupation: "Software Engineer",
+        location: "San Francisco, USA"
+    }
+];
+
+export default users;
---
+++ src/carData.js
@@ -0,0 +1,52 @@
+const cars = [
+    {
+        id: 1,
+        name: "mersedes G",
+        brand: "mersedes",
+        model: "G klass",
+        year: 2023,
+        img_url: "https://www.topgear.com/sites/default/files/2024/05/1-Mercedes-G-Class-review.jpg?w=976&h=549",
+        price: "$75,000",
+        color: "gray",
+        type: "Electric",
+        location: "California, USA"
+    },
+    {
+        id: 2,
+        name: "Mustang",
+        brand: "Ford",
+        model: "Mustang GT",
+        year: 2019,
+        img_url: "https://hips.hearstapps.com/hmg-prod/images/2024-ford-mustang-gt-111-64e6608fce997.jpg?crop=0.639xw:0.538xh;0.00801xw,0.375xh&resize=2048:*",
+        price: "$55,000",
+        color: "Blue",
+        type: "Sports",
+        location: "Texas, USA"
+    },
+    {
+        id: 3,
+        name: "bmw x",
+        brand: "bmw",
+        model: "x5",
+        year: 2019,
+        img_url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/BMW/X5-2023/10452/1688992642182/front-left-side-47.jpg?tr=w-664",
+        price: "$23,000",
+        color: "black",
+        type: "Sedan",
+        location: "Tokyo, Japan"
+    },
+    {
+        id: 4,
+        name: "porshee",
+        brand: "porshe",
+        model: "911",
+        year: 2021,
+        img_url: "https://i.ebayimg.com/images/g/gfkAAOSwFTxkMb0L/s-l1600.webp",
+        price: "$500,000",
+        color: "Yellow",
+        type: "Supercar",
+        location: "Rome, Italy"
+    }
+];
+
+export default cars;
